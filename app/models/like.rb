71b9f0fe3dabfcac65440456f3a8f038c084e988
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-05-04 15:25:56 +0500

    Remove linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
   def update_like_count
     post.increment!(:likes_counter)
   end
