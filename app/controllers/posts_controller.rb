2f7292bf838f76e629c0b64a632da59bdd355063
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-05-10 20:21:32 +0500

    Update Post and USer controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
-  def index; end
 
-  def show; end
+  def index
+    @posts = Post.where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
+  end
+
+ 
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
 end
