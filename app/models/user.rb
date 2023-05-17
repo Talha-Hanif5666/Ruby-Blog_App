1d1a7810ff04e3931fbe6dfa701a39ae22f84efa
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-05-17 20:53:01 +0500

    Remove linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :confirmable, :validatable
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
