03212d3a09f246ce7a41267aaff8b061e75e0123
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-05-17 20:51:27 +0500

    Remove duplicate devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :confirmable, :validatable
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
