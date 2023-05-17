b0be99269a5e16aaec85143f4ae2042a3aaddda6
MosDevx <mosesk.wamae@gmail.com>
2023-05-17 18:14:25 +0300

    Add route for devise and scope of user

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-   # Defines the root path route ("/")
-  # root "articles#index"
-  root "users#index"
+  
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+    root to: 'devise/sessions#new'
+  end  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
   end
-end
+ end
