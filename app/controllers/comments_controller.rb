d3117ecb9a0ca8982e0750d4062baa514cf9c38a
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-05-18 21:43:29 +0500

    Add destrot method to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,4 +12,11 @@ class CommentsController < ApplicationController
 
     redirect_to user_posts_path(@user, @post)
   end
+  def destroy
+    @comment = Comment.find(params[:id])
+    authorize! :destroy, @comment
+    @comment.destroy
+    redirect_to post_path(@comment.post), notice: "Comment deleted successfully."
+  end
+  
 end
